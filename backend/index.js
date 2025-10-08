3f742e765e6bc0d4b85975fdbeab03115343d8ed
kamlesh-chandel <kamlesh@gkmit.co>
2025-10-08 11:00:54 +0530

    chore: updated cors origin policy for live url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://email-sender-app-pied.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     Credentials: true
 }))
